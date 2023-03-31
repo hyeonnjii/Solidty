742f097da208965d378079d487f36b256cf55d0e
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2023-03-31 19:26:25 +0900

    Update lec4_function.sol

--- lec4_function.sol
+++ lec4_function.sol
@@ -4,23 +4,23 @@ pragma solidity >= 0.7.0 < 0.9.0;
 contract lec4{
 
     /*
-    function 이름 () public { // (public, private, internal, external) 변경가능.
+    function 이름 () public { // (public, private, internal, external) 접근제어 변경가능.
         // 내용
     }
     */
 
-    uint256 public a = 3;
-    //1. Parameter 와 return 값이 없는 function 정의
+    uint256 public a = 3; // 기초값
+    //1. Parameter 와 return 값이 없는 function 정의 > 5값 a에 대입
     function changeA1() public{
        a=5;
     }
 
-    //2. parameter는 있고, return 값이 없는 function 정의
+    //2. parameter는 있고, return 값이 없는 function 정의 > parameter type을 결정해줘야 
     function changeA2(uint256 _value) public{
         a =_value;
     }
 
-    //3. parameter는 있고, return 값도 있는 function 정의
+    //3. parameter는 있고, return 값도 있는 function 정의 > returns! 꼭 붙여주고 type도 넣어줘야 함
     function changeA3(uint256 _value) public returns(uint256){
         a=_value;
         return a;
