0a5d6af7cedf73735dfea51d8b024f7a6bc3d2af
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2023-03-31 19:36:10 +0900

    Update lec5_public, private 등.sol

--- lec5_public, private 등.sol
+++ lec5_public, private 등.sol
@@ -23,7 +23,24 @@ contract lec5{
     //2. private > 우리가 배포한 곳은 smart contract 내가 아니라 밖이기 때문에 a2가 보이지 않음, 접근 불가
     uint256 private a2 = 5;
 
-    //ex. instance 를 사용하는데, 해당 instance 가 private일 경우 error가 남
+    /*ex. instance 를 사용하는데, 해당 instance 가 private일 경우 error가 남
+    instance는 우리가 다른 contract 에 접근하기 위해서는 접근하려는 contract에 다른 contract을 instance 화 시켜야 함
+    
+    contract Public_ex {
+      unit256 public a = 3;
+      
+      function changeA(uint256 _value) public {
+            a = _value;
+       }
+    }
+    
+    contract Public_ex2 {
+    
+      Public_ex instance = new Public_ex()
+      
+      function changeA_2(uint256 _value) public {
+            instance.changeA(_value);
+       }
     // ex. external 함수를 사용하면 다른 함수가 해당 함수를 instance로 사용할 때 가능! 자기자신이 아니니까
     
 }
