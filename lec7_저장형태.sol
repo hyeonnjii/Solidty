7f9de386bd9a3d7f783f4815388527dfc9ad5762
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2023-03-31 19:49:56 +0900

    Update lec7_저장형태.sol

--- lec7_저장형태.sol
+++ lec7_저장형태.sol
@@ -14,15 +14,15 @@ Colldata : 주로 external function 의 파라메터에서 사용 됩니다.
 stack:  EVM (Ethereum Virtual Machine) 에서 stack data를 관리할때 쓰는 영역인데 1024Mb 제한적입니다.이 컨트랙을 상속받은 자식 컨트랙도 접근가능)
 */
 
-//string은 기본 데이터 타입이 아니라서, 저장 형태 정해줘야 해
+//string은 기본 데이터 타입이 아니라서, memory 라는 키워드를 붙여줘야 해> 저장 형태 정해줘야 해
 //저장에 따라 gas값이 달라지기 때문
 
 contract lec7 {
     
-    function get_string(string memory _str) public pure returns( string memory){
+    function get_string(string memory _str) public pure returns(string memory){
         return _str;
     }
 }
 
 // uint 의 경우는 기본 데이터 타입이기 때문에 memory 가 default 값으로 설정이 되있기 때문에
-// 따로 설정안해도 됨
+// 따로 설정안해도 됨(memory는 default 값으로 되어있기 때문에 굳이 안넣어도 deploy 되긴 함)
