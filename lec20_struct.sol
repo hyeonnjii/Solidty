6e49d8c4770ae6e50d08ad6e0ce90a15bd9d22cc
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2023-03-31 20:50:20 +0900

    Update lec20_struct.sol

--- lec20_struct.sol
+++ lec20_struct.sol
@@ -3,20 +3,20 @@ pragma solidity >= 0.7.0 < 0.9.0;
 
 // 구조체를 만들어 mapping, array에 넣어 사용할 수 있음
 contract lec20{
-    struct Character{
+    struct Character{ // 3가지를 인풋값으로 꼭 넣어줘야 함
         uint256 age;
         string name;
         string job;
     }
     
-    mapping(uint256=>Character) public CharacterMapping;
-    Character[] public CharacterArray;
+    mapping(uint256=>Character) public CharacterMapping; // 구조체 타입을 결과로 mapping
+    Character[] public CharacterArray; // character라는 구조체 타입을 넣어 배열을 생성
     
     function createCharacter(uint256 _age,string memory _name,string memory _job) pure public returns(Character memory) {
         return Character(_age,_name,_job);
     }
     
-    function createChracterMapping(uint256 _key, uint256 _age,string memory _name,string memory _job )  public {
+    function createChracterMapping(uint256 _key, uint256 _age,string memory _name, string memory _job )  public {
        CharacterMapping[_key] = Character(_age,_name,_job);
     }
     
